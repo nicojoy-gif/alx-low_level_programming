7e60b9f529418fe00041fa567cff0082255cec9e
nicojoy-gif <orjionyinyechi080@gmail.com>
2022-03-10 12:44:16 -0800

    printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
-int.main(void)
+
+/**
+* main - print the string in the put
+*
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
+
+int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
