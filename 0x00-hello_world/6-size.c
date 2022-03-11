2fc82bafdf1875aa821dee6b470ed4a766a7fe51
nicojoy-gif <orjionyinyechi080@gmail.com>
2022-03-10 22:12:03 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,7 +6,6 @@
 * Description: using the main function
 * this program prints "Programming is like building a multilingual puzzle
 * Return: 0
-*
 */
 int main(void)
 {
@@ -23,7 +22,7 @@ sizeof(i));
 	printf("Size of a long int: %ld byte(s)\n",
 sizeof(li));
 	printf("Size of a long long int: %ld byte(s)\n",
-sizeof(lli);
+sizeof(lli));
 	printf("Size of a float: %ld byte(s)\n",
 sizeof(f));
 	return (0);
