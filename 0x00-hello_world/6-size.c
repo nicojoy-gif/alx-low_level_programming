9dabc1a62c88fdeeca713eb5fece6b57c3164c14
nicojoy-gif <orjionyinyechi080@gmail.com>
2022-03-10 20:34:07 -0800

    add

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,11 +18,12 @@ int main(void)
 
 	printf("Size of a char: %ld byte(s)\n",
 sizeof(c));
-	printf("Size of a int: %ld byte(s)\n",
+	printf("Size of an int: %ld byte(s)\n",
 sizeof(i));
-	printf("Size of a long: %ld byte(s)\n",   
-sizeof(li));                                      
-	printf("Size of a long long: %ld byte(s)\n", sizeof(lli));  
+	printf("Size of a long int: %ld byte(s)\n",
+sizeof(li));
+	printf("Size of a long long: %ld byte(s)\n",
+sizeof(lli);
 	printf("Size of a float: %ld byte(s)\n",
 sizeof(f));
 	return (0);
