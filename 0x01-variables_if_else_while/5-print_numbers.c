b130a912306917a1ea142fefd392dfb72c224d72
nicojoy-gif <orjionyinyechi080@gmail.com>
2022-03-11 14:24:24 -0800

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - A program that prints all single digit number
+* of base 10 followed by a new line
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int num;
+	int d;
+
+	scanf("%d", &num);
+
+	while (temp > 0)
+	{
+		d = n % 10;
+		n = n / 10;
+		printf("%d\n", d);
+	}
+	return (0);
+}
