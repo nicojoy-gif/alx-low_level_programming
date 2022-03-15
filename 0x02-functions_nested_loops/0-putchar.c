0d3bc2170dd76a9befff304fccdd3090f4c123b6
nicojoy-gif <orjionyinyechi080@gmail.com>
2022-03-15 07:01:31 -0700

    print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - this program prints _putchar
+* foloowed by a new line
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char n;
+
+	n = _putchar;
+	putchar(n, '\n');
+	return (0);
+}
