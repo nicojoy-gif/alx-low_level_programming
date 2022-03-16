c4c9da1bbb33217329e2c379908edfd2d13f1f14
nicojoy-gif <orjionyinyechi080@gmail.com>
2022-03-16 09:11:30 -0700

    print time

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * void jack_bauer(void) - entry point
+ * Description: prints every minute of the day
+ * starting from 00:00 to 23:59
+ * Return: nothing
+ */
+void jack_bauer(void)
+{
+	int x = 0;
+	int j = 0;
+
+	while (x <= 23)
+	{
+		j = 0;
+		while (j <= 59)
+		{
+			_putchar((x / 10) + '0');
+			_putchar((x % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
+			j += 1;
+		}
+		x += 1;
+	}
+}
