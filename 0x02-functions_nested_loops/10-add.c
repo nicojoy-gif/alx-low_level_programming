56ed3621a6d17250cdd0532fe29010038ee8ff08
nicojoy-gif <orjionyinyechi080@gmail.com>
2022-03-16 10:28:21 -0700

    add integer

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - entry point function thata adds two integer.
+ * @n1: input integer 1
+ * @n2: input integer 2
+ * Return: Always 0 (Success)
+ */
+int add(int n1, int n2)
+{
+	int res;
+
+	res = n1 + n2;
+	return (res);
+}
