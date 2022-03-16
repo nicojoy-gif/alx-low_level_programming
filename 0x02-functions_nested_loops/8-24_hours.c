b0c16139960c4872f09b8e498d6016870ecf3484
nicojoy-gif <orjionyinyechi080@gmail.com>
2022-03-16 09:15:06 -0700

    check time

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * void jack_bauer(void) - entry point
+ * jack_bauer - entry point
  * Description: prints every minute of the day
  * starting from 00:00 to 23:59
  * Return: nothing
