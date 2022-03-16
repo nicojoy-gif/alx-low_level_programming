cfda4c57cfcd49ac7287835bb20568f9dbb53f0f
nicojoy-gif <orjionyinyechi080@gmail.com>
2022-03-16 05:12:02 -0700

    check lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _islower - entry point
+ * Description: checks for lowercase,
+ * if c is lowercase return 1
+ * Return: Always 0 (Success)
+ */
+int _islower(int c)
+{
+	c = 'c';
+
+	if (_islower(c))
+	{
+		putchar(1);
+	}
+		else
+		{
+			putchar(0);
+		}
+	return (0);
+}
