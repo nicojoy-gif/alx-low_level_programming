cb22bfa33f91fad88758f2993788699a88c8ec0a
nicojoy-gif <orjionyinyechi080@gmail.com>
2022-03-16 10:09:00 -0700

    print 9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * times_table - entry point
+ * Description: It prints 9 times table
+ * Return: nothing
+ */
+void times_table(void)
+{
+	int row, col;
+	int mult = 0;
+
+	for (row = 0; row < 10; row++)
+	{
+
+		for (col = 0; col < 10; col++)
+		{
+			mult = (row * col);
+			if (mult < 10)
+			{
+				if (col != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar(mult + '0');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar((mult / 10) + '0');
+				_putchar((mult % 10) + '0');
+			}
+			if (col != 9)
+				_putchar(',');
+
+		}
+			_putchar('\n');
+	}
+}
