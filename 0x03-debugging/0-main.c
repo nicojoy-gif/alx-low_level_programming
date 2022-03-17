06cff537d9f648e9345478a6d0c91190dbe97912
nicojoy-gif <orjionyinyechi080@gmail.com>
2022-03-16 21:08:48 -0700

    first debugging task on positive or negative number

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Return: nothing
+ */
+void positive_or_negative(int i)
+{
+
+	i = 98;
+	positive_or_negative(i);
+
+}
