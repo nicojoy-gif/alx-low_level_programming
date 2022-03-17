d24451eaf5283143719142f195fdeba1e212a874
nicojoy-gif <orjionyinyechi080@gmail.com>
2022-03-16 21:53:35 -0700

    debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,12 +2,14 @@
 
 /**
  * main - tests function that prints if integer is positive or negative
- * Return: nothing
+ * Return: 0
  */
-void positive_or_negative(int i)
+int main(void)
 {
+	int i;
 
-	i = 98;
+	i = 0;
 	positive_or_negative(i);
 
+	return (0);
 }
