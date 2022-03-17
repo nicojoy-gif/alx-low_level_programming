21579e981b3e63ac74a98aaba84e72b161910602
nicojoy-gif <orjionyinyechi080@gmail.com>
2022-03-17 12:35:27 -0700

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,6 +4,14 @@
  * print_numbers - entry point
  * Description: it prints numbers from 0 to 9
  * followed by a new line
+ * _putchar: function used
  * Return: nothing
  */
+void print_numbers(void)
+{
+	int i;
 
+	for (i = 0; i <= 10; i++)
+		_putchar(i + '0');
+	_putchar('\n');
+}
