6890f4e55876b6bb5c05d79ed4bd2988eb7d3d1a
nicojoy-gif <orjionyinyechi080@gmail.com>
2022-03-17 20:53:18 -0700

    print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - entry point
+ * Description: print square
+ * @size: variable used
+ * Return: nothing
+ */
+void print_square(int size)
+{
+	int files;
+	int columns;
+
+	if (size > 8)
+		for (files = 0; files < size; files++)
+		{
+			for (columns = 0; columns < size; columns++)
+			{
+				_putchar('0');
+			}
+			_putchar('\n');
+		}
+	else
+	{
+		_putchar('\n');
+	}
+}
