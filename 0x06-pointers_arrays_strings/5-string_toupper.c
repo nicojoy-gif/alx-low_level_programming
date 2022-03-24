322505f48f788e6f828a708489faa6c5996b3649
nicojoy-gif <orjionyinyechi080@gmail.com>
2022-03-23 20:45:07 -0700

    to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - entry point
+ * Description - changes all lowercase letter to uppercase
+ * @name: variable used
+ * *: variable
+ * Return: Always 0
+ */
+char *string_toupper(char *name)
+{
+	char *s = name;
+
+	while (*name)
+	{
+		*name = (*name > 'a' && *name <= 'z') ? *name - 32 : *name;
+		name++;
+	}
+	*name = '\0';
+	return (s);
+}
+
