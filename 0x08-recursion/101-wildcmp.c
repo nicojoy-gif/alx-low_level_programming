ea591fb01f15755b84ea4d535007533861e42805
nicoratty <orjionyinyechi080@gmail.com>
2022-03-30 12:38:30 -0700

    wildcmp

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * wildcmp - entry point
+ * Description - compare two strings allowing for wildcard char
+ * @s1: variable
+ * @s2: variable
+ * Return: 1 if identical, 0 otherwise
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+			return (wildcmp(s1, s2 + 1));
+		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
+			return (1);
+	}
+	return (0);
+}
