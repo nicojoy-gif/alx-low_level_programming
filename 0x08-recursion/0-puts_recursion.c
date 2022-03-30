a460b6afe5bf823f78f0e4f574334dd319323951
nicoratty <orjionyinyechi080@gmail.com>
2022-03-30 01:59:00 -0700

    print recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - entry point
+ * Description - prints a string followed by a new line
+ * @s: variable used
+ * Return: nothing
+ */
+void _puts_recursion(char *s)
+{
+	if (!*s == '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	_putchar('\n');
+}
