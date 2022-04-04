1f4d6b9aef065380f9d5139524ff9e46c17f526b
nicoratty <orjionyinyechi080@gmail.com>
2022-04-04 13:08:41 -0700

    concatenate two argument

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+
+/**
+ * *argstostr - entry point
+ * Description - concatenate all arguments of your program
+ * @ac: variable used
+ * @av: variable used
+ * Return: NULL if ac == 0 or av ==NULL
+ */
+char *argstostr(int ac, char **av)
+{
+	char *str;
+	int i, j, k, len;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	i = len = 0;
+	while (i < ac)
+	{
+		j = 0;
+		while (av[i][j] != '\0')
+		{
+			len++;
+			j++;
+		}
+		len++;
+		i++;
+	}
+	len++;
+	str = malloc(len * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	i = k = 0;
+	while (i < ac)
+	{
+		j = 0;
+		while (av[i][j] != 0)
+		{
+			str[k] = av[i][j];
+			k++;
+			j++;
+		}
+		str[k] = '\n';
+		k++;
+		i++;
+	}
+	return (str);
+}
