816ff9b5f96ae682b089c5d35be7ff359bb78b11
nicoratty <orjionyinyechi080@gmail.com>
2022-04-11 04:03:40 -0700

    define a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+/**
+ * struct dog -  type of def
+ * @name: character used
+ * @age: float used
+ * @owner: character used
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
