6c3f63693c1ca25ac0a9597159793c59d9010a8d
nicoratty <orjionyinyechi080@gmail.com>
2022-04-11 05:51:32 -0700

    header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,15 +1,24 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef DOG_H
+#define DOG_H
 /**
  * struct dog -  type of def
  * @name: character used
  * @age: float used
  * @owner: character used
+ *
+ * *Description: structure to store dog info
  */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
 #endif
