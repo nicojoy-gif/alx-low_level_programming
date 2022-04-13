ca1d8304ba04d164243cdc889cccf52b5630b21b
nicoratty <orjionyinyechi080@gmail.com>
2022-04-13 04:48:35 -0700

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - entry point
+ * Description - prints a name
+ * @f: variabele used
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f == NULL)
+		return;
+	f(name);
+}
