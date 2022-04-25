288db97ecc92a72b8e46054a5b7d949da20f56f5
nicoratty <orjionyinyechi080@gmail.com>
2022-04-25 14:44:32 -0700

    add new element to end of list

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * *add_nodeint_end - function to add node from top
+ * @head: int type of node struct pointer
+ * @n: const int type
+ * Return: Always success
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *top = NULL;
+	listint_t *temp = NULL;
+
+	top = malloc(sizeof(listint_t));
+	if (top == NULL)
+		return (NULL);
+	top->n = n;
+	if (*head == NULL)
+	{
+		*head = top;
+		top->next = NULL;
+		return (*head);
+	}
+	temp = *head;
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+	}
+	temp->next = top;
+	top->next = NULL;
+	return (*head);
+}
